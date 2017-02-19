c2f12b5a18844578ac6e56fc2c74a8aae7bb5ddd
Todd <chihsuanwu@outlook.com>
2017-02-19 23:03:11 +0800

    optimize board

--- chessboard.cpp
+++ chessboard.cpp
@@ -1,5 +1,6 @@
 #include "chessboard.hpp"
 #include <stdio.h>
+#include <boo>
 #include <assert.h>
 
 #define CHESSBOARD_DIMEN 15
@@ -8,77 +9,85 @@
 
 enum STATUS{EMPTY, BLACK, WHITE};
 
-ChessBoard::ChessBoard(){
-
-  /* initialize pointStatus */
-  for (int r = 0; r < CHESSBOARD_DIMEN; r++)
-    for (int c = 0; c < CHESSBOARD_DIMEN; c++)
-      pointStatus[r][c] = EMPTY;
-
+void ChessBoard::ChessBoard()
+{
+    wipe(false);
 }
 
 /* prints the current chesssboard */
-ChessBoard::invalidate()
+void ChessBoard::invalidate()
 {
-  for (int r = 0; r < CHESSBOARD_DIMEN; r++)
-    for (int c = 0; c < CHESSBOARD_DIMEN; c++)
-      switch (pointStatus[r][c])
-      {
-        case EMPTY:
-          if (r == 0)
-          {
-            if (c == 0)
-              printf("┌");
-            else if (c == CHESSBOARD_DIMEN)
-              printf("┐\n");
-            else
-              printf("┬");
-          }
-          else if (r == CHESSBOARD_DIMEN)
-          {
-            if (c == 0)
-              printf("└");
-            else if (c == CHESSBOARD_DIMEN)
-              printf("┘\n");
-            else
-              printf("┴");
-          }
-          else
-          {
-            if (c == 0)
-              printf("├");
-            else if (c == CHESSBOARD_DIMEN)
-              printf("┤\n");
-            else
-              printf("┼");
-          }
-          break;
-        case BLACK:
-          printf(CHESS_BLACK);
-          break;
-        case WHITE:
-          printf(CHESS_WHITE);
-          break;
-      }
+    for (int r = 0; r < CHESSBOARD_DIMEN; r++)
+        for (int c = 0; c < CHESSBOARD_DIMEN; c++)
+        {
+            switch (pointStatus[r][c])
+            {
+                case BLACK:
+                    printf(CHESS_BLACK); break;
+                case WHITE:
+                    printf(CHESS_WHITE); break;
+                default:
+                    switch (r)
+                    {
+                        case 0:
+                            switch (c)
+                            {
+                                case 0:
+                                    printf("┌"); break;
+                                case CHESSBOARD_DIMEN - 1:
+                                    printf("┐"); break;
+                                default:
+                                    printf("┬");
+                            }
+                            break;
+                        case CHESSBOARD_DIMEN - 1:
+                            switch (c)
+                            {
+                                case 0:
+                                    printf("└"); break;
+                                case CHESSBOARD_DIMEN - 1:
+                                    printf("┘"); break;
+                                default:
+                                    printf("┴");
+                            }
+                            break;
+                        default:
+                            switch (c)
+                            {
+                                case 0:
+                                    printf("├"); break;
+                                case CHESSBOARD_DIMEN - 1:
+                                    printf("┤"); break;
+                                default:
+                                    printf("┼");
+                            }
+                            break;
+                    }
+            }
+
+            if (c == CHESSBOARD_DIMEN - 1)
+                printf("\n");
+        }
 }
 
 /* puts a new chess */
 void ChessBoard::play(STATUS color, int row, int col)
 {
-  if (pointStatus[row][col] != EMPTY)
-    assert(0); 
-  else
-    pointStatus[row][col] = color;
-
-  invalidate();
+    if (pointStatus[row][col] != EMPTY)
+        assert(0); 
+    else
+        pointStatus[row][col] = color;
+    
+    invalidate();
 }
 
 /* clears the whole game */
-void ChessBoard::wipe()
+void ChessBoard::wipe(bool invalidate)
 {
-  for (int i = 0; i < CHESSBOARD_DIMEN; i++)
-    for (int k = 0; k < CHESSBOARD_DIMEN; k++)
-      pointStatus[i][k] = EMPTY;
+    for (int i = 0; i < CHESSBOARD_DIMEN; i++)
+        for (int k = 0; k < CHESSBOARD_DIMEN; k++)
+            pointStatus[i][k] = EMPTY;
 
-  invalidate();
+        if (invalidate)
+            invalidate();
 }
