04aa0e2569368524fef9cdddf91ebb53bc870408
pinyen <pinyen@pinyen-VirtualBox.(none)>
2017-02-19 23:13:59 +0800

    create chessboard.hpp

---
+++ chessboard.hpp
@@ -0,0 +1,20 @@
+#define CHESSBOARD_DIMEN 15
+class ChessBoard{
+
+public:
+  enum STATUS{EMPTY, BLACK, WHITE};
+
+  ChessBoard();
+
+  /* redraw the current chesssboard */
+  void invalidate();
+
+  /* puts a new chess */
+  void play(STATUS color, int row, int col);
+
+  /* clears the whole game */
+  void wipe();
+
+private:
+  STATUS pointStatus[CHESSBOARD_DIMEN][CHESSBOARD_DIMEN];
+};
