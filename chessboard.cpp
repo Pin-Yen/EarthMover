6b778904f4a1116af4b21e5ca07a3ebb3edd1121
pinyen <pinyen@pinyen-VirtualBox.(none)>
2017-02-19 22:44:21 +0800

    fix syntax error (add return type)

--- chessboard.cpp
+++ chessboard.cpp
@@ -8,7 +8,7 @@
 
 enum STATUS{EMPTY, BLACK, WHITE};
 
-ChessBoard::ChessBoard(){
+void ChessBoard::ChessBoard(){
 
   /* initialize pointStatus */
   for (int r = 0; r < CHESSBOARD_DIMEN; r++)
@@ -18,7 +18,7 @@ ChessBoard::ChessBoard(){
 }
 
 /* prints the current chesssboard */
-ChessBoard::invalidate()
+void ChessBoard::invalidate()
 {
   for (int r = 0; r < CHESSBOARD_DIMEN; r++)
     for (int c = 0; c < CHESSBOARD_DIMEN; c++)
