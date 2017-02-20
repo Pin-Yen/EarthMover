391a62ba0e1f93f42c2c9c06baa03c83e5c6175a
pinyen <pinyen@pinyen-VirtualBox.(none)>
2017-02-20 12:30:47 +0800

    change to g++

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-gcc -c -std=c++98 main.cpp -o main.o
-gcc -c -std=c++98 chessboard.cpp -o chessboard.o
-gcc -std=c++98 chessboard.o main.o -o FiveInARow
+g++ -c -std=c++98 main.cpp -o main.o
+g++ -c -std=c++98 chessboard.cpp -o chessboard.o
+g++ -std=c++98 chessboard.o main.o -o FiveInARow
