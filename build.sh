b0b14247d84b4611d673b3b4d725b0cd05217066
pinyen <pinyen@pinyen-VirtualBox.(none)>
2017-02-20 13:36:45 +0800

    change to c++11

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-g++ -c -std=c++98 main.cpp -o main.o
-g++ -c -std=c++98 chessboard.cpp -o chessboard.o
-g++ -std=c++98 chessboard.o main.o -o FiveInARow
+g++ -c -std=c++11 main.cpp -o main.o
+g++ -c -std=c++11 chessboard.cpp -o chessboard.o
+g++ -std=c++11 chessboard.o main.o -o FiveInARow
