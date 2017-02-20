1939d81b4eafedeee5057ec6287a8ac0026c687c
Todd <chihsuanwu@outlook.com>
2017-02-20 09:09:54 +0800

    fix argument error

--- chessboard.hpp
+++ chessboard.hpp
@@ -1,20 +1,21 @@
 #define CHESSBOARD_DIMEN 15
-class ChessBoard{
 
-public:
-  enum STATUS{EMPTY, BLACK, WHITE};
+class ChessBoard
+{
+  public:
+    enum STATUS{EMPTY, BLACK, WHITE};
 
-  ChessBoard();
+    ChessBoard();
 
-  /* redraw the current chesssboard */
-  void invalidate();
+    /* redraw the current chesssboard */
+    void invalidate();
 
-  /* puts a new chess */
-  void play(STATUS color, int row, int col);
+    /* puts a new chess */
+    void play(STATUS color, int row, int col);
 
-  /* clears the whole game */
-  void wipe();
+    /* clears the whole game */
+    void wipe(bool invalidate);
 
-private:
-  STATUS pointStatus[CHESSBOARD_DIMEN][CHESSBOARD_DIMEN];
+  private:
+    STATUS pointStatus[CHESSBOARD_DIMEN][CHESSBOARD_DIMEN];
 };
