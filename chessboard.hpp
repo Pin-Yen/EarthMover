cce2255a548c95749df66cb83b7ee6f27dee7f33
Todd <chihsuanwu@outlook.com>
2017-02-21 10:04:09 +0800

    fix tab

--- chessboard.hpp
+++ chessboard.hpp
@@ -5,42 +5,42 @@
 
 class ChessBoard
 {
-  public:
+public:
 
-    #ifndef STATUS_ENUM
-    #define STATUS_ENUM
-    enum STATUS{EMPTY, BLACK, WHITE};
-    #endif
+  #ifndef STATUS_ENUM
+  #define STATUS_ENUM
+  enum STATUS{EMPTY, BLACK, WHITE};
+  #endif
 
-    ChessBoard();
+  ChessBoard();
 
     /* print the current chesssboard */
-    void invalidate();
+  void invalidate();
 
     /* puts a new chess, if the ponit is not empty then return false*/
-    bool play(STATUS color, int row, int col);
+  bool play(STATUS color, int row, int col);
 
     /* clears the whole game */
-    void wipe(bool isInvalidate);
+  void wipe(bool isInvalidate);
 
     /* get is black turn or not*/
-    bool isBlackTurn();
+  bool isBlackTurn();
 
     /* search the whole board for winning conditions */
-    STATUS judge();
+  STATUS judge();
 
-  private:
-    const char CHESS_BLACK = 'X', CHESS_WHITE = 'O';
+private:
+  const char CHESS_BLACK = 'X', CHESS_WHITE = 'O';
 
-    STATUS pointStatus[CHESSBOARD_DIMEN][CHESSBOARD_DIMEN];
+  STATUS pointStatus[CHESSBOARD_DIMEN][CHESSBOARD_DIMEN];
 
-    std::array<Point, CHESSBOARD_DIMEN * CHESSBOARD_DIMEN> records;
+  std::array<Point, CHESSBOARD_DIMEN * CHESSBOARD_DIMEN> records;
 
-    bool blackTurn;
+  bool blackTurn;
 
     /* the total number of plays */
-    int playNo;
+  int playNo;
 
     /* print a part of the board*/
-    void printBoard(int row, int col, STATUS chess);
+  void printBoard(int row, int col, STATUS chess);
 };
