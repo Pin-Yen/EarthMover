8af2431252715e9abdf0bea52c3196a6da04a7f9
Todd <chihsuanwu@outlook.com>
2017-02-24 22:40:03 +0800

    fix tab width

--- typetree.hpp
+++ typetree.hpp
@@ -3,41 +3,41 @@
 class TypeTree
 {
 public:
-	TypeTree();
-	~TypeTree();
+  TypeTree();
+  ~TypeTree();
 
-	enum STATUS 
-	{
-		EMPTY = (int)' ', SAME = (int)'O', DIFFERENT = (int)'X', 
-		NO_MATTER = (int)'-', ANALYZE_POINT = (int)'*'
-	};
+  enum STATUS 
+  {
+    EMPTY = (int)' ', SAME = (int)'O', DIFFERENT = (int)'X', 
+    NO_MATTER = (int)'-', ANALYZE_POINT = (int)'*'
+  };
 
-	ChessType* classify(STATUS *status);
+  ChessType* classify(STATUS *status);
 
 private:
 
-	const int length = 9;
-	const bool checkLongConnect = false;
+  const int length = 9;
+  const bool checkLongConnect = false;
 
-	struct Node{
+  struct Node{
 
-		/* Next point occupied by:
-		 * 0:same color, 1:different color, 2:empty */
-		Node *childNode[3];
+    /* Next point occupied by:
+     * 0:same color, 1:different color, 2:empty */
+    Node *childNode[3];
 
-		struct ChessType *type;
-	};
+    struct ChessType *type;
+  };
 
-	Node root;
+  Node root;
 
-	/* Depth First Search
-	 * parameters of the initial call should be:
-	 * currentLocation: length/2, move = -1 */
-	void dfs(Node *root, STATUS *status, int currentLocation, int move);
+  /* Depth First Search
+   * parameters of the initial call should be:
+   * currentLocation: length/2, move = -1 */
+  void dfs(Node *root, STATUS *status, int currentLocation, int move);
 
-	/* copied from chesstypemaker.cpp, just for debugging purposes */
-	void print(int length, STATUS *status, ChessType *style);
+  /* copied from chesstypemaker.cpp, just for debugging purposes */
+  void print(int length, STATUS *status, ChessType *style);
 
-	/* copied from chesstypemaker.cpp */
-	ChessType * typeAnalyze(STATUS *status, bool checkForbidden);
+  /* copied from chesstypemaker.cpp */
+  ChessType * typeAnalyze(STATUS *status, bool checkForbidden);
 };
