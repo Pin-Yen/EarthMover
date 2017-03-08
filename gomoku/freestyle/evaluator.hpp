7cb5641bce363108501a2980a525dde6b497eee1
Todd <chihsuanwu@outlook.com>
2017-03-08 14:37:29 +0800

    remove all about forbidden

--- gomoku/freestyle/evaluator.hpp
+++ gomoku/freestyle/evaluator.hpp
@@ -6,6 +6,4 @@ class Evaluator
 {
 public:
   static void evaluate(ChessType* type[4][2], STATUS *status, int dir, int *score);
-
-  static void evaluate_forbidden(ChessType* type[4][2], STATUS *status, int dir, int *score);
 };
