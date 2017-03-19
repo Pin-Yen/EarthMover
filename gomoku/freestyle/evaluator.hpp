02bd2f479d72eda418488eb3359beec55c4324d7
Pin-Yen <pinyentraffic@gmail.com>
2017-03-19 15:15:28 +0800

    Change parameter order

--- gomoku/freestyle/evaluator.hpp
+++ gomoku/freestyle/evaluator.hpp
@@ -6,7 +6,7 @@ class Evaluator {
  public:
   /* inputs status array in *status.
    * analyzes both player's chesstype, and stores black's type in type[0], white's in type[1] */
-  static void evaluate_type(ChessType* type[2], STATUS *status);
+  static void evaluate_type(STATUS *status, ChessType* type[2]);
 
   /* inputs an array of ChessTypes of both players in type[4][2],
    * returns black's score in score[0], white's in score[1] */
