42192e88884eb78802dbece1d1924b10be15178f
Todd <chihsuanwu@outlook.com>
2017-03-23 09:50:55 +0800

    using copy constructor to new a ChessType in classify

--- gomoku/freestyle/typetree.cpp
+++ gomoku/freestyle/typetree.cpp
@@ -134,12 +134,10 @@ void TypeTree::classify(STATUS *status, ChessType *(type[2])) {
 
       /* if reach leaf, return type */
       if (node->type[0] != NULL) {
-        //type[0] = node->type[0];
-        //type[1] = node->type[1];
         if (type[0] != NULL) delete type[0];
         if (type[1] != NULL) delete type[1];
-        type[0] = new ChessType(node->type[0]->length, node->type[0]->life);
-        type[1] = new ChessType(node->type[1]->length, node->type[1]->life);
+        type[0] = new ChessType(node->type[0]);
+        type[1] = new ChessType(node->type[1]);
 
         return;
       }
