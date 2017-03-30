aac06ac3fff4588b54e5c3c7337bca0c41635189
Todd <chihsuanwu@outlook.com>
2017-03-30 20:03:07 +0800

    fix bug: forgot to add playNo when winning

--- gomoku/freestyle/virtualboard.cpp
+++ gomoku/freestyle/virtualboard.cpp
@@ -156,9 +156,9 @@ bool VirtualBoard::getHSP(int &row, int &col) {
 }
 
 bool VirtualBoard::play(int row, int col) {
-  if (score[row][col][(playNo & 1)] == 10000000) return true;
-
   ++playNo;
+
+  if (score[row][col][(playNo & 1)] == 10000000) return true;
 
   STATUS color = ((playNo & 1) ? BLACK : WHITE);
 
