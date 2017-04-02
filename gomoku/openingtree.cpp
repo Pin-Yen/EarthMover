e1d3f14b0607ec8d4c3b10f6f11d169693a22cb3
Todd <chihsuanwu@outlook.com>
2017-04-02 14:42:34 +0800

    update opening tree

--- gomoku/openingtree.cpp
+++ gomoku/openingtree.cpp
@@ -1,1 +1,10 @@
 #include "openingtree.hpp"
+
+/* initialize root*/
+OpeningTree::Node* OpeningTree::root = new Node();
+bool OpeningTree::isInitialized = false;
+
+void OpeningTree::initialize() {
+  if (isInitialized) return;
+  isInitialized = true;
+}
--- gomoku/openingtree.hpp
+++ gomoku/openingtree.hpp
@@ -2,7 +2,23 @@
 #define OPENING_TREE_H
 
 class OpeningTree {
+ public:
+  static void initialize();
 
-}
+ private:
+  struct Node {
+    Node *childNode[][];
+
+    double score;
+
+    Node() {
+
+    }
+  };
+
+  static Node* root;
+
+  static bool isInitialized;
+};
 
 #endif
