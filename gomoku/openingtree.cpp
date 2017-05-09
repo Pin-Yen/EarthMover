95877a2be60e0bbb2ef647743e55f7f1812f384d
Todd <chihsuanwu@outlook.com>
2017-05-09 15:41:22 +0800

    add comment

--- gomoku/openingtree.cpp
+++ gomoku/openingtree.cpp
@@ -71,7 +71,9 @@ void OpeningTree::initialize() {
                           {0, 0, 0, 0, 0, 0, 0},  /*               */
                           {0, 0, 0, 0, 0, 0, 0}}};/*               8*/
 
-  for (int i = 0; i < 1; ++i) {
+
+  for (int i = 0; i < 8; ++i) {
+    // insert 8 direction
     for (int r = 0; r < 4; ++r) {
       insert(tables[i]);
       rotate(tables[i]);
@@ -86,6 +88,7 @@ void OpeningTree::initialize() {
 
 void OpeningTree::rotate(int table[7][7]) {
   int temp[7][7];
+  /* rotate 90 degrees clockwise (row -> col, col -> 7 - row) */
   for (int r = 0; r < 7; ++r)
     for (int c = 0; c < 7; ++c)
       temp[c][7 - r] = table[r][c];
@@ -97,6 +100,7 @@ void OpeningTree::rotate(int table[7][7]) {
 
 void OpeningTree::mirror(int table[7][7]) {
   int temp[7][7];
+  /* mirror (row -> col, col -> row) */
   for (int r = 0; r < 7; ++r)
     for (int c = 0; c < 7; ++c)
       temp[c][r] = table[r][c];
@@ -111,6 +115,14 @@ void OpeningTree::insert(int table[7][7]) {
 
   int oriRow = 0, oriCol = 0, curRow, curCol, color;
 
+  /* set the origin in the lower right corner in the table
+     e.g. the origin of the example below is "0"
+          /*   X           */
+          /*   C O O C     */
+          /*     C X 0     */
+          /*               */
+          /*               */
+          /*               */
   for (int r = 0; r < 7; ++r)
     for (int c = 0; c < 7; ++c)
       if (table[r][c] > 0 && table[r][c] < 3) {
@@ -120,9 +132,11 @@ void OpeningTree::insert(int table[7][7]) {
           oriCol = c;
       }
 
+  /* set current point to origin */
   curRow = oriRow; curCol = oriCol;
 
   while (true) {
+    /* the insert order is from right to left, from bottom to top */
     --curCol;
     if (curCol < 0) {
       if (curRow == 0) break;
@@ -131,26 +145,32 @@ void OpeningTree::insert(int table[7][7]) {
       curCol = 6;
     }
 
+    /* if find a occupied point */
     if (table[curRow][curCol] > 0 && table[curRow][curCol] < 3) {
       color = table[curRow][curCol] - 1;
+
+      /* using vector type to record (origin to current point) */
       if (currentNode->childNode[oriRow - curRow][oriCol - curCol][color] == NULL)
         currentNode->childNode[oriRow - curRow][oriCol - curCol][color] = new Node();
       currentNode = currentNode->childNode[oriRow - curRow][oriCol - curCol][color];
     }
   }
 
+  /* record the fifth move's score */
   for (curRow = 0; curRow < 7; ++curRow)
     for (curCol = 0; curCol < 7; ++curCol)
       if (table[curRow][curCol] >= 3) {
+        /* using vector type to record
+           to avoid out of bound (vector may be negative), all value += 3 */
         if (currentNode->childNode[oriRow - curRow + 3][oriCol - curCol + 3][0] == NULL)
           currentNode->childNode[oriRow - curRow + 3][oriCol - curCol + 3][0] = new Node();
-        else
-          continue;
+
         currentNode->childNode[oriRow - curRow + 3][oriCol - curCol + 3][0]->score =
           table[curRow][curCol];
       }
 }
 
+/* classify method please refer to insert */
 void OpeningTree::classify(int table[7][7]) {
   Node* currentNode = root;
 
@@ -187,6 +207,7 @@ void OpeningTree::classify(int table[7][7]) {
   for (int curRow = 0; curRow < 7; ++curRow)
     for (int curCol = 0; curCol < 7; ++curCol)
       if (currentNode->childNode[curRow][curCol][0] != NULL)
+        // debug purpose, print result
         std::cout << curCol - 3 << ", "
                   << curRow - 3 << " :"
                   << currentNode->childNode[curRow][curCol][0]->score
@@ -199,27 +220,31 @@ OpeningTree::Node::Node() {
       childNode[r][c][0] = NULL;
       childNode[r][c][1] = NULL;
     }
+
+  score = 0;
 }
 
 int main()
 {
   OpeningTree::initialize();
 
-  int test[7][7] = {{0, 0, 0, 0, 0, 0, 0},  /*               */
-                    {0, 0, 0, 0, 0, 0, 0},  /*               */
-                    {0, 0, 0, 0, 0, 0, 0},  /*               */
-                    {0, 1, 0, 2, 0, 0, 0},  /*  X   O        */
-                    {0, 0, 2, 0, 0, 0, 0},  /*    O          */
-                    {0, 0, 0, 1, 0, 0, 0},  /*      X        */
-                    {0, 0, 0, 0, 0, 0, 0}}; /*               */
+  int test1[7][7] = {{0, 0, 0, 0, 0, 0, 0},  /*               */
+                     {0, 0, 0, 0, 0, 0, 0},  /*               */
+                     {0, 0, 0, 0, 0, 0, 0},  /*               */
+                     {0, 0, 1, 0, 0, 0, 0},  /*    X          */
+                     {0, 0, 2, 0, 0, 0, 0},  /*    O          */
+                     {0, 1, 0, 2, 0, 0, 0},  /*  X   O        */
+                     {0, 0, 0, 0, 0, 0, 0}}; /*               */
 
-  int tes2[7][7] = {{0, 0, 0, 0, 0, 0, 0},  /*               */
-                    {0, 0, 0, 0, 0, 0, 0},  /*               */
-                    {0, 0, 0, 0, 0, 0, 0},  /*               */
-                    {0, 2, 0, 1, 0, 0, 0},  /*  O   X        */
-                    {0, 0, 2, 0, 0, 0, 0},  /*    O          */
-                    {0, 1, 0, 0, 0, 0, 0},  /*  X            */
-                    {0, 0, 0, 0, 0, 0, 0}}; /*               */
+  int test2[7][7] = {{0, 0, 0, 0, 0, 0, 0},  /*               */
+                     {0, 0, 0, 0, 0, 0, 0},  /*               */
+                     {0, 0, 0, 0, 0, 0, 0},  /*               */
+                     {0, 2, 0, 1, 0, 0, 0},  /*  O   X        */
+                     {0, 0, 2, 0, 0, 0, 0},  /*    O          */
+                     {0, 1, 0, 0, 0, 0, 0},  /*  X            */
+                     {0, 0, 0, 0, 0, 0, 0}}; /*               */
 
-  OpeningTree::classify(tes2);
+  OpeningTree::classify(test1);
+  std::cout << std::endl;
+  OpeningTree::classify(test2);
 }
