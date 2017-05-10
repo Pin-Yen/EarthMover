a70ffcba03624ff9b91e96af55e62cff010e3ac9
Todd <chihsuanwu@outlook.com>
2017-05-10 15:35:36 +0800

    fix logical bug, add new opening, reduce table size to 5x5

--- gomoku/openingtree.cpp
+++ gomoku/openingtree.cpp
@@ -1,6 +1,5 @@
 #include "openingtree.hpp"
 
-#include <assert.h>
 #include <iostream>
 #include <fstream>
 
@@ -12,9 +11,11 @@ void OpeningTree::initialize() {
 
   int openingAmout;
   while (file >> openingAmout) {
-    char table[7][7];
-    for (int r = 0; r < 7; ++r)
-      for (int c = 0; c < 7; ++c)
+    std::cout << "inserting opening " << openingAmout << std::endl;
+
+    char table[5][5];
+    for (int r = 0; r < 5; ++r)
+      for (int c = 0; c < 5; ++c)
         file >> table[r][c];
 
     /* insert 8 direction
@@ -29,35 +30,34 @@ void OpeningTree::initialize() {
   }
 
   std::cout << "finished insert " << openingAmout << " opening table" << std::endl;
-
   file.close();
 }
 
-void OpeningTree::rotate(char table[7][7]) {
-  char temp[7][7];
-  /* rotate 90 degrees clockwise (row -> col, col -> 6 - row) */
-  for (int r = 0; r < 7; ++r)
-    for (int c = 0; c < 7; ++c)
-      temp[c][6 - r] = table[r][c];
+void OpeningTree::rotate(char table[5][5]) {
+  char temp[5][5];
+  /* rotate 90 degrees clockwise (row -> col, col -> 4 - row) */
+  for (int r = 0; r < 5; ++r)
+    for (int c = 0; c < 5; ++c)
+      temp[c][4 - r] = table[r][c];
 
-  for (int r = 0; r < 7; ++r)
-    for (int c = 0; c < 7; ++c)
+  for (int r = 0; r < 5; ++r)
+    for (int c = 0; c < 5; ++c)
       table[r][c] = temp[r][c];
 }
 
-void OpeningTree::mirror(char table[7][7]) {
-  char temp[7][7];
+void OpeningTree::mirror(char table[5][5]) {
+  char temp[5][5];
   /* mirror (row -> col, col -> row) */
-  for (int r = 0; r < 7; ++r)
-    for (int c = 0; c < 7; ++c)
+  for (int r = 0; r < 5; ++r)
+    for (int c = 0; c < 5; ++c)
       temp[c][r] = table[r][c];
 
-  for (int r = 0; r < 7; ++r)
-    for (int c = 0; c < 7; ++c)
+  for (int r = 0; r < 5; ++r)
+    for (int c = 0; c < 5; ++c)
       table[r][c] = temp[r][c];
 }
 
-void OpeningTree::insert(char table[7][7]) {
+void OpeningTree::insert(char table[5][5]) {
   Node* currentNode = root;
 
   int oriRow = 0, oriCol = 0, curRow, curCol, color;
@@ -70,8 +70,8 @@ void OpeningTree::insert(char table[7][7]) {
           /*               */
           /*               */
           /*               */
-  for (int r = 0; r < 7; ++r)
-    for (int c = 0; c < 7; ++c)
+  for (int r = 0; r < 5; ++r)
+    for (int c = 0; c < 5; ++c)
       if (table[r][c] == 'X' || table[r][c] == 'O') {
         if (r > oriRow)
           oriRow = r;
@@ -89,12 +89,12 @@ void OpeningTree::insert(char table[7][7]) {
       if (curRow == 0) break;
       --curRow;
 
-      curCol = 6;
+      curCol = 4;
     }
 
     /* if find a occupied point */
     if (table[curRow][curCol] == 'X' || table[curRow][curCol] == 'O') {
-      color = table[curRow][curCol] - 1;
+      color = table[curRow][curCol] == 'X' ? 0 : 1;
 
       /* using vector type to record (origin to current point) */
       if (currentNode->childNode[oriRow - curRow][oriCol - curCol][color] == NULL)
@@ -104,24 +104,24 @@ void OpeningTree::insert(char table[7][7]) {
   }
 
   /* record the fifth move's score */
-  for (curRow = 0; curRow < 7; ++curRow)
-    for (curCol = 0; curCol < 7; ++curCol)
+  for (curRow = 0; curRow < 5; ++curRow)
+    for (curCol = 0; curCol < 5; ++curCol)
       if (table[curRow][curCol] == 'P') {
         /* using vector type to record
-           to avoid out of bound (vector may be negative), all value += 3 */
-        if (currentNode->childNode[oriRow - curRow + 3][oriCol - curCol + 3][0] == NULL)
-          currentNode->childNode[oriRow - curRow + 3][oriCol - curCol + 3][0] = new Node();
+           to avoid out of bound (vector may be negative), all value ++ */
+        if (currentNode->childNode[oriRow - curRow + 1][oriCol - curCol + 1][0] == NULL)
+          currentNode->childNode[oriRow - curRow + 1][oriCol - curCol + 1][0] = new Node();
       }
 }
 
 /* classify method please refer to insert */
-void OpeningTree::classify(char table[7][7]) {
+void OpeningTree::classify(char table[5][5]) {
   Node* currentNode = root;
 
   int oriRow = 0, oriCol = 0, curRow, curCol, color;
 
-  for (int r = 0; r < 7; ++r)
-    for (int c = 0; c < 7; ++c)
+  for (int r = 0; r < 5; ++r)
+    for (int c = 0; c < 5; ++c)
       if (table[r][c] == 'X' || table[r][c] == 'O') {
         if (r > oriRow)
           oriRow = r;
@@ -137,36 +137,34 @@ void OpeningTree::classify(char table[7][7]) {
       if (curRow == 0) break;
       --curRow;
 
-      curCol = 6;
+      curCol = 4;
     }
 
     if (table[curRow][curCol] == 'X' || table[curRow][curCol] == 'O') {
-      color = table[curRow][curCol] - 1;
+      color = table[curRow][curCol] == 'X' ? 0 : 1;
       if (currentNode->childNode[oriRow - curRow][oriCol - curCol][color] == NULL) return;
 
       currentNode = currentNode->childNode[oriRow - curRow][oriCol - curCol][color];
     }
   }
 
-  for (int curRow = 0; curRow < 7; ++curRow)
-    for (int curCol = 0; curCol < 7; ++curCol)
+  for (int curRow = 0; curRow < 5; ++curRow)
+    for (int curCol = 0; curCol < 5; ++curCol)
       if (currentNode->childNode[curRow][curCol][0] != NULL)
         // debug purpose, print result
-        std::cout << curCol - 3 << ", " << curRow - 3 << std::endl;
+        std::cout << curCol - 1 << ", " << curRow - 1 << std::endl;
 }
 
 int main()
 {
   OpeningTree::initialize();
-
   std::ifstream file("openingtest.txt");
 
-  std::cout << "test:\n";
   int testAmount;
   while (file >> testAmount) {
-    char table[7][7];
-    for (int r = 0; r < 7; ++r) {
-      for (int c = 0; c < 7; ++c) {
+    char table[5][5];
+    for (int r = 0; r < 5; ++r) {
+      for (int c = 0; c < 5; ++c) {
         file >> table[r][c];
         std::cout << table[r][c];
       }
--- gomoku/openingtree.hpp
+++ gomoku/openingtree.hpp
@@ -7,26 +7,26 @@ class OpeningTree {
  public:
   static void initialize();
 
-  static void classify(char table[7][7]);
+  static void classify(char table[5][5]);
 
  private:
   struct Node {
     Node() {
-      for (int r = 0; r < 7; ++r)
-        for (int c = 0; c < 7; ++c) {
+      for (int r = 0; r < 5; ++r)
+        for (int c = 0; c < 5; ++c) {
           childNode[r][c][0] = NULL;
           childNode[r][c][1] = NULL;
         }
     }
 
-    Node *childNode[7][7][2];
+    Node *childNode[5][5][2];
   };
 
-  static void rotate(char table[7][7]);
+  static void rotate(char table[5][5]);
 
-  static void mirror(char table[7][7]);
+  static void mirror(char table[5][5]);
 
-  static void insert(char table[7][7]);
+  static void insert(char table[5][5]);
 
   static Node* root;
 };
