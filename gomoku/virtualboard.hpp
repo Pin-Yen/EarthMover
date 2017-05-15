ee6f01c29f237f1330003dc7115c6bb182a96380
Todd <chihsuanwu@outlook.com>
2017-05-15 16:48:17 +0800

    add vb in gomoku (super class of freestyle/renju's vb)

---
+++ gomoku/virtualboard.hpp
@@ -0,0 +1,46 @@
+#ifndef GOMOKU_VIRTUAL_BOARD_H
+#define GOMOKU_VIRTUAL_BOARD_H
+
+class VirtualBoard {
+ public:
+  static const int CHESSBOARD_DIMEN = 15;
+
+  VirtualBoard();
+  /* copy the source board to consturct the board */
+  VirtualBoard(VirtualBoard* source);
+
+  ~VirtualBoard();
+
+  inline int getScore(int row, int col);
+
+  /* get the sume of every point's score */
+  int getScoreSum();
+
+  /* get who turn, black = 0, white = 1 */
+  bool whoTurn() { return (playNo_ & 1); }
+
+  /* get the highest score's position, if every point is not empty, return false */
+  bool getHSP(int &row, int &col);
+
+  /* puts a new chess at (row ,col),
+   * returns 1 if wins after play, -1 if lose */
+  int play(int row, int col);
+ private:
+  /* nested class */
+  class Point;
+  class Evaluator;
+
+  /* point array */
+  Point* point_[CHESSBOARD_DIMEN][CHESSBOARD_DIMEN];
+
+  /* the total number of plays */
+  int playNo_;
+};
+
+#include "point.hpp"
+
+inline int VirtualBoard::getScore(int row, int col) {
+  return point_[row][col]->getScore();
+}
+
+#endif
