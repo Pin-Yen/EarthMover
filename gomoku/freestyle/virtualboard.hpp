0149b02bcb1914da6bf76e328a91af90b9d1ea41
Todd <chihsuanwu@outlook.com>
2017-05-16 20:35:14 +0800

    forgot to add public in VBFS

--- gomoku/freestyle/virtualboard.hpp
+++ gomoku/freestyle/virtualboard.hpp
@@ -2,6 +2,7 @@
 #define GOMOKU_FREESTYLE_VIRTUAL_BOARD_H
 
 class VirtualBoardFreeStyle : public VirtualBoardGomoku<8, VirtualBoard::Evaluator> {
+ public:
   VirtualBoardFreeStyle() : VirtualBoardGomoku() {}
   VirtualBoardFreeStyle(VirtualBoardFreeStyle* board) : VirtualBoardGomoku(board) {}
 
