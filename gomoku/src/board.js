c8941ae45801debbf399f8144c346a3f74254ff3
Todd <chihsuanwu@outlook.com>
2017-06-07 17:35:16 +0800

    create board.js include Board object

---
+++ gomoku/src/board.js
@@ -0,0 +1,224 @@
+var Board = function() {
+  this.enable = false;
+  this.gameStarted = false;
+
+  this.display = { playNumber: false, coordinate: false };
+
+  this.mousePos = [-1, -1];
+  this.lastPlay = [-1, -1];
+
+  this.canvas = document.getElementById('cvs');
+  this.context = this.canvas.getContext("2d");
+  var cvs = $(this.canvas);
+  cvs.attr({ 'width': 565, 'height': 565 });
+
+  cvs.click(this.click.bind(this));
+
+  cvs.mousemove(this.mouseMoveOrOver.bind(this));
+  cvs.mouseover(this.mouseMoveOrOver.bind(this));
+
+  cvs.mouseout(this.mouseOut.bind(this));
+
+  this.playNo = 0;
+
+  // board status array
+  this.status = new Array(15);
+  for (var i = this.status.length - 1; i >= 0; i--)
+    this.status[i] = new Array(15);
+
+  function createImage(src) {
+    var image = new Image();
+    image.src = 'gomoku/src/chess_' + src + '.png';
+    return image;
+  }
+
+  this.image = {
+    'black': {
+      normal: createImage('black'),
+      transparent: createImage('black_transparent'),
+      marked : createImage('black_marked')
+    },
+    'white': {
+      normal: createImage('white'),
+      transparent: createImage('white_transparent'),
+      marked : createImage('white_marked')
+    }
+  };
+};
+
+Board.prototype.init = function() {
+  this.playNo = 0;
+
+  for (var row = this.status.length - 1; row >= 0; row--)
+    for (var col = this.status[row].length - 1; col >= 0; col--)
+      this.status[row][col] = 0;
+
+  this.mousePos = [-1, -1];
+  this.lastPlay = [-1, -1];
+
+  this.context.clearRect(20, 20, 525, 525);
+};
+
+Board.prototype.mouseMoveOrOver = function(event) {
+  // get position
+  var rect = this.canvas.getBoundingClientRect();
+  var scaling = this.canvas.scrollWidth / 565;
+
+  var x = Math.floor((event.clientX - rect.left - 20 * scaling)  / (35 * scaling)),
+      y = Math.floor((event.clientY - rect.top - 20 * scaling) / (35 * scaling));
+
+  if (x < 0 || x > 14 || y < 0 || y > 14) {
+    x = -1; y = -1;
+  }
+
+  var pos = [x, y];
+
+  // if at new position
+  if (this.mousePos === pos) return;
+
+  // check if not out of bound and position is empty
+  if (pos[0] != -1 && !this.status[pos[0]][pos[1]]) {
+    if (this.enable) {
+      this.clear(this.mousePos);
+      this.draw(pos);
+    }
+    // update mousePos
+    this.mousePos = pos.slice();
+  } else {
+    // out of bound or at an occupyed position, clear and set position to (-1, -1)
+    if (this.enable) this.clear(this.mousePos);
+
+    this.mousePos = [-1, -1];
+  }
+}
+
+Board.prototype.mouseOut = function() {
+  // if mouse's previous position not has chess
+  if (this.mousePos[0] != -1) {
+    // clear the previous image
+    this.clear(this.mousePos);
+
+    // set mousePos to -1, -1
+    this.mousePos = [-1, -1];
+  }
+}
+
+Board.prototype.click = function() {
+  // if the game does not started, show new game dialog
+  if (!this.gameStarted) {
+    $('#dialog-new-game').show();;
+    return;
+  }
+
+  if (!this.enable) return;
+
+  if (this.mousePos[0] == -1) return;
+
+  // if the position is empty, play and post
+  if (!this.status[this.mousePos[0]][this.mousePos[1]]) {
+    this.play(this.mousePos);
+    post({ row: this.mousePos[1], col: this.mousePos[0] }, 'play');
+    this.mousePos = [-1, -1];
+  }
+}
+
+// draw iamge at position
+Board.prototype.draw = function(pos) {
+  if (pos[0] == -1) return;
+
+  var status = this.status[pos[0]][pos[1]];
+  var image;
+  if (status == 0) {
+    image = this.image[this.whoTurn()].transparent;
+  } else if (status == this.playNo) {
+    image = this.display.playNumber ? this.image[this.whoTurn(status - 1)].normal :
+                              this.image[this.whoTurn(status - 1)].marked;
+  } else {
+    image = this.image[this.whoTurn(status - 1)].normal;
+  }
+
+  this.context.drawImage(image, pos[0] * 35 + 21, pos[1] * 35 + 21, 33, 33);
+
+  if (this.display.playNumber && status > 0) {
+    if (status == this.playNo)
+      this.context.fillStyle = 'red';
+    else {
+      this.context.fillStyle = ((status - 1) & 1) ? 'black' : 'white';
+    }
+    this.context.font = '29px Ubuntu';
+    this.context.textAlign = 'center';
+
+    this.context.fillText(status, pos[0] * 35 + 37, pos[1] * 35 + 47, 27);
+  }
+}
+
+// clear the position
+Board.prototype.clear = function(pos) {
+  if (pos[0] == -1) return;
+
+  this.context.clearRect(pos[0] * 35 + 21, pos[1] * 35 + 21, 33, 33);
+}
+
+// play at position, we should make sure that position is empty
+Board.prototype.play = function(pos) {
+  ++this.playNo;
+  this.status[pos[0]][pos[1]] = this.playNo;
+
+  // draw a marked chess
+  this.draw(pos);
+
+  // redraw last play's image (remove the mark)
+  this.clear(this.lastPlay);
+  this.draw(this.lastPlay);
+
+  // update last play
+  this.lastPlay = pos.slice(0);
+
+  // lock board
+  this.enable = false;
+
+  // stop timer
+  timer[this.whoTurn(this.playNo - 1)].stop();
+}
+
+Board.prototype.whoTurn = function(param) {
+  var num = param === undefined ? this.playNo : param;
+  return (num & 1) ? 'white' : 'black';
+}
+
+Board.prototype.changePlayNumber = function() {
+  this.display.playNumber = !this.display.playNumber;
+
+  for (var row = this.status.length - 1; row >= 0; row--)
+    for (var col = this.status[row].length - 1; col >= 0; col--)
+      if (this.status[row][col] > 0) {
+        this.clear([row, col]);
+        this.draw([row, col]);
+      }
+
+  return this.display.playNumber;
+}
+
+Board.prototype.changeCoordinate = function() {
+  this.display.coordinate = !this.display.coordinate;
+
+  if (this.display.coordinate) {
+    this.context.fillStyle = '#444';
+    this.context.font = '12px Ubuntu';
+    this.context.textAlign = 'center';
+    for (var i = 1; i <= 15; ++i) {
+      var text = String.fromCharCode(64 + i);
+      this.context.fillText(text, i * 35 + 2, 15);
+      this.context.fillText(text, i * 35 + 2, 558);
+      this.context.fillText(i, 10, i * 35 + 7);
+      this.context.fillText(i, 552, i * 35 + 7);
+    }
+  } else {
+    this.context.clearRect(0, 20, 20, 525);
+    this.context.clearRect(545, 20, 20, 525);
+    this.context.clearRect(20, 0, 525, 20);
+    this.context.clearRect(20, 545, 525, 20);
+  }
+
+  return this.display.coordinate;
+};
--- gomoku/src/main.js
+++ gomoku/src/main.js
@@ -1,187 +1,11 @@
-var timer = { black: null, white: null };
+var timer = { 'black': null, 'white': null };
 
-var game = { black: 'human', white: 'computer'};
+var game = { 'black': 'human', 'white': 'computer'};
 
-var boardEnable = false, gameStarted = false;
-var playNumber = false;
-var coordinate = false;
-
-var mousePos = [-1, -1], lastPlay = [-1, -1];
-
-// image array
-var chessImage = new Array(6);
-for (var i = chessImage.length - 1; i >= 0; i--)
-  chessImage[i] = new Image();
-
-chessImage[0].src = "gomoku/src/chess_black_transparent.png";
-chessImage[1].src = "gomoku/src/chess_black.png";
-chessImage[2].src = "gomoku/src/chess_black_marked.png";
-chessImage[3].src = "gomoku/src/chess_white_transparent.png";
-chessImage[4].src = "gomoku/src/chess_white.png";
-chessImage[5].src = "gomoku/src/chess_white_marked.png";
-
-var canvas = document.getElementById('cvs');
-var context = canvas.getContext("2d");
-$(canvas).attr({ 'width': 565, 'height': 565 });
-
-var playNo = 0;
-
-// board status array
-var boardStatus = new Array(15);
-for (var i = boardStatus.length - 1; i >= 0; i--)
-  boardStatus[i] = new Array(15);
-
-// initialize board
-function initBoard() {
-  playNo = 0;
-
-  for (var row = boardStatus.length - 1; row >= 0; row--)
-    for (var col = boardStatus[row].length - 1; col >= 0; col--)
-      boardStatus[row][col] = 0;
-
-  mousePos = [-1, -1];
-  lastPlay = [-1, -1];
-
-  context.clearRect(20, 20, 525, 525);
-}
-
-function cvsMouseMoveOrOver(event) {
-  // get the coordinate
-  var position = getPosition(event);
-
-  // if at new position
-  if (mousePos === position) return;
-
-  // check if not out of bound
-  if (position[0] != -1) {
-    // check if the position is empty
-    if (!boardStatus[position[0]][position[1]]) {
-      if (boardEnable) {
-        clear(mousePos);
-        draw(position);
-      }
-      // update mousePos
-      mousePos = position.slice();
-
-      return;
-    }
-  }
-
-  // out of bound or at an occupyed position, clear and set position to (-1, -1)
-  if (boardEnable) clear(mousePos);
-  mousePos = [-1, -1];
-}
-
-function cvsMouseOut() {
-  // if mouse's previous position not has chess
-  if (mousePos[0] != -1) {
-    // clear the previous image
-    clear(mousePos);
-
-    // set mousePos to -1, -1
-    mousePos = [-1, -1];
-  }
-}
-
-function cvsClick() {
-  // if the game does not started, show new game dialog
-  if (!gameStarted) {
-    $('#dialog-new-game').show();;
-    return;
-  }
-
-  if (!boardEnable) return;
-
-  if (mousePos[0] == -1) return;
-
-  // if the position is empty, play and post
-  if (!boardStatus[mousePos[0]][mousePos[1]]) {
-    play(mousePos);
-    post({ row: mousePos[1], col: mousePos[0] }, 'play');
-    mousePos = [-1, -1];
-  }
-}
-
-// get position array [x, y]
-function getPosition(event) {
-  var rect = canvas.getBoundingClientRect();
-  var scaling = canvas.scrollWidth / 565;
-
-  var x = Math.floor((event.clientX - rect.left - 20 * scaling)  / (35 * scaling)),
-      y = Math.floor((event.clientY - rect.top - 20 * scaling) / (35 * scaling));
-
-  if (x < 0 || x > 14 || y < 0 || y > 14) {
-    x = -1; y = -1;
-  }
-
-  return [x, y];
-}
-
-// draw iamge at position
-function draw(pos) {
-  if (pos[0] == -1) return;
-
-  var status = boardStatus[pos[0]][pos[1]];
-  var image;
-  if (status == 0) {
-    image = chessImage[(playNo & 1) * 3];
-  } else if (status == playNo) {
-    image = playNumber ? chessImage[((status - 1) & 1) * 3 + 1] :
-                         chessImage[((status - 1) & 1) * 3 + 2];
-  } else {
-    image = chessImage[((status - 1) & 1) * 3 + 1];
-  }
-
-  context.drawImage(image, pos[0] * 35 + 21, pos[1] * 35 + 21, 33, 33);
-
-  if (playNumber && status > 0) {
-    if (status == playNo)
-      context.fillStyle = 'red';
-    else {
-      context.fillStyle = ((status - 1) & 1) ? 'black' : 'white';
-    }
-    context.font = '29px Ubuntu';
-    context.textAlign = 'center';
-
-    context.fillText(status, pos[0] * 35 + 37, pos[1] * 35 + 47, 27);
-  }
-}
-
-// clear the position
-function clear(pos) {
-  if (pos[0] == -1) return;
-
-  context.clearRect(pos[0] * 35 + 21, pos[1] * 35 + 21, 33, 33);
-}
-
-// play at position, we should make sure that position is empty
-function play(position) {
-  ++playNo;
-  boardStatus[position[0]][position[1]] = playNo;
-
-  // draw a marked chess
-  draw(position);
-
-  // redraw last play's image (remove the mark)
-  clear(lastPlay);
-  draw(lastPlay);
-
-  // update last play
-  lastPlay = position.slice(0);
-
-  // lock board
-  boardEnable = false;
-
-  // stop timer
-  if (playNo & 1)
-    timer.black.stop();
-  else
-    timer.white.stop();
-}
+var board = new Board();
 
 function notifyWinner(winnerColor) {
   // TODO: display game status
-  boardEnable = false;
   setTimeout(function() { alert(winnerColor + " wins !"); }, 0);
 }
 
@@ -198,30 +22,29 @@ function post(params, path) {
 
       if (path == 'think') {
         // play at ai's respond point
-        play([response.col, response.row]);
+        board.play([response.col, response.row]);
 
-        if (response.col == mousePos[0] && response.row == mousePos[1])
-          mousePos = [-1, -1];
+        if (response.col == board.mousePos[0] && response.row == board.mousePos[1])
+          board.mousePos = [-1, -1];
       }
 
       // check if someone win
       if (response.winner != -1) {
         notifyWinner(response.winner == 0 ? 'Black' : 'White');
+        board.enable = false;
+        board.gameStarted = false;
         return;
       }
     }
 
     if (humanTurn()) {
-      draw(mousePos);
-      boardEnable = true;
+      board.draw(board.mousePos);
+      board.enable = true;
     } else {
       post(null, 'think');
     }
 
-    if (playNo & 1)
-      timer.white.start();
-    else
-      timer.black.start();
+    timer[board.whoTurn()].start();
   };
 
 
@@ -229,8 +52,7 @@ function post(params, path) {
 }
 
 function humanTurn() {
-  return ((playNo & 1) == 0 && game.black == 'human') ||
-          ((playNo & 1) == 1 && game.white == 'human');
+  return game[board.whoTurn()] == 'human';
 }
 
 // initialize player
@@ -269,20 +91,20 @@ function btnDialogOKClick() {
   if ($('#dl-name-white').val().trim() == '')
     $('#dl-name-white').val('you');
 
-  game = {black: black, white: white};
+  game = {'black': black, 'white': white};
 
   var rule = parseInt($('input[name="rule"]:checked').val(), 10), level = parseInt($('#dl-select').val(), 10);
 
   post({rule: rule, level: level}, 'start');
 
   // deside player's place
-  if (game.black == 'human') {
+  if (black == 'human') {
     // black == human, white == human -> sel: black, human, opp: white, human
     // black == human, white == ai -> sel: black, human, opp: white, ai
     initPlayer('sel', 'black', true);
-    initPlayer('opp', 'white', (game.white == 'human'));
+    initPlayer('opp', 'white', (white == 'human'));
   } else {
-    if (game.white == 'human') {
+    if (white == 'human') {
       // black == ai, white == human -> sel: white, human, opp: black, ai
       initPlayer('sel', 'white', true);
       initPlayer('opp', 'black', false);
@@ -291,13 +113,13 @@ function btnDialogOKClick() {
       initPlayer('sel', 'black', false);
       initPlayer('opp', 'white', false);
     }
-    boardEnable = false;
   }
 
-  gameStarted = true;
+  board.enable = false;
+  board.gameStarted = true;
 
   // initialize board
-  initBoard();
+  board.init();
 
   // initialize timer
   if (timer.black != null) timer.black.stop();
@@ -308,42 +130,17 @@ function btnDialogOKClick() {
 }
 
 function btnPlayNumberClick() {
-  playNumber = !playNumber;
-
-  if (playNumber) {
+  if (board.changePlayNumber()) {
     $('#play-number-check').show();
   } else {
     $('#play-number-check').hide();
   }
-
-  for (var row = boardStatus.length - 1; row >= 0; row--)
-    for (var col = boardStatus[row].length - 1; col >= 0; col--)
-      if (boardStatus[row][col] > 0) {
-        clear([row, col]);
-        draw([row, col]);
-      }
 }
 
 function btnCoordinateClick() {
-  coordinate = !coordinate;
-
-  if (coordinate) {
+  if (board.changeCoordinate()) {
     $('#coordinate-check').show();
-    context.fillStyle = '#444';
-    context.font = '12px Ubuntu';
-    context.textAlign = 'center';
-    for (var i = 1; i <= 15; ++i) {
-      var text = String.fromCharCode(64 + i);
-      context.fillText(text, i * 35 + 2, 15);
-      context.fillText(text, i * 35 + 2, 558);
-      context.fillText(i, 10, i * 35 + 7);
-      context.fillText(i, 552, i * 35 + 7);
-    }
   } else {
     $('#coordinate-check').hide();
-    context.clearRect(0, 20, 20, 525);
-    context.clearRect(545, 20, 20, 525);
-    context.clearRect(20, 0, 525, 20);
-    context.clearRect(20, 545, 525, 20);
   }
 }
