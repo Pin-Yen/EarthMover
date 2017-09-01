2e68724c7652c66c8cc0266f221e3995d289ba44
Todd <chihsuanwu@outlook.com>
2017-09-01 17:41:07 +0800

    update cnntrainer.py

--- neuralnetwork/go/cnntrainer.py
+++ neuralnetwork/go/cnntrainer.py
@@ -4,49 +4,49 @@ import numpy as np
 class Network(object):
     def __init__(self, depth=10, width=48):
         self.sess = tf.Session()
-        # Network's depth and width
+        # Network's depth and width.
         self.depth = depth
         self.width = width
 
-
+    # Set up network structure.
     def setUp(self):
-        # Input: 19x19x3, output: 361
+        # Input: 19x19x3, output: 361.
         self.x = tf.placeholder(tf.float32, [None, 19, 19, 3])
         self.y = tf.placeholder(tf.float32, [None, 361])
 
         # Each layers is composed of conv and bn layer
-        # First layer
+        # First layer.
         with tf.name_scope('Layer1'):
             conv1 = tf.layers.conv2d(
                 self.x, self.width, 3, padding='same',
-                activation=tf.nn.relu, name='conv')
-            bn1 = tf.layers.batch_normalization(conv1, name='bn')
+                activation=tf.nn.relu, name='conv1')
+            bn1 = tf.layers.batch_normalization(conv1, name='bn1')
 
-        # Hidden layer
+        # Hidden layer.
         currentLayer = bn1
-        for i in xrange(2, self.depth + 1):
+        for i in range(2, self.depth + 1):
             with tf.name_scope('Layer' + str(i)):
                 conv = tf.layers.conv2d(currentLayer, self.width, 3,
-                    padding = 'same', activation = tf.nn.relu, name='conv')
-                bn = tf.layers.batch_normalization(conv, name='bn')
+                    padding = 'same', activation = tf.nn.relu, name='conv' + str(i))
+                bn = tf.layers.batch_normalization(conv, name='bn' + str(i))
             currentLayer = bn
 
-        # Output layer
-        with tf.name_scope('Output layer'):
+        # Output layer.
+        with tf.name_scope('Layer' + str(self.depth + 1)):
             conv1x1 = tf.layers.conv2d(currentLayer, 1, 1,
                 padding = 'same', activation = tf.nn.relu, name='conv1x1')
             output = tf.nn.softmax(tf.reshape(conv1x1, [-1, 361]))
 
-        # Loss function
+        # Loss function.
         with tf.name_scope('Loss'):
             self.loss = tf.losses.softmax_cross_entropy(
                 onehot_labels = self.y, logits = output)
             tf.summary.scalar('loss', self.loss)
 
-        # Optimizer
+        # Optimizer.
         self.opt = tf.train.AdamOptimizer(1e-4).minimize(self.loss)
 
-        # Accuracy
+        # Accuracy.
         with tf.name_scope('Accuracy'):
             correct_prediction = tf.equal(tf.argmax(output, 1),
                                           tf.argmax(self.y, 1))
@@ -54,26 +54,40 @@ class Network(object):
                 tf.cast(correct_prediction, tf.float32))
             tf.summary.scalar('accuracy', self.accuracy)
 
+        # Summary.
         self.merged = tf.summary.merge_all()
-        self.writer = tf.summary.FileWriter("logs/", self.sess.graph)
+        self.trainWriter = tf.summary.FileWriter("logs/train", self.sess.graph)
+        self.testWriter = tf.summary.FileWriter("logs/test", self.sess.graph)
 
+
+    # Initialize variable.
     def initVar(self):
         self.sess.run(tf.global_variables_initializer())
 
 
-    def train(self, data, batchSize=128):
-        for i in range(data.dataSize // batchSize):
-            batch_x, batch_y = data.getBatch(i, batchSize)
-            self.sess.run([self.opt],
-                {self.x: batch_x, self.y: batch_y})
+    # Train network.
+    def train(self, trainData, testData=None, batchSize=128):
+        for i in range(trainData.dataSize // batchSize):
+            print('mini batch ' + str(i), end='\r')
+            # Get in/out data.
+            x, y = trainData.getBatch(i, batchSize)
+            self.sess.run([self.opt], {self.x: x, self.y: y})
+
+            # Upgrade accuracy to summary.
             if i % 10 == 0:
                 summ = self.sess.run(self.merged,
-                        {self.x: batch_x, self.y: batch_y})
+                        {self.x: x, self.y: y})
 
-                self.writer.add_summary(summ, i)
+                self.trainWriter.add_summary(summ, i)
+                if testData != None:
+                    testX, testY = testData.getBatch(0, 128)
+                    testSumm = self.sess.run(self.merged,
+                        {self.x: testX, self.y: testY})
+
+                    self.testWriter.add_summary(testSumm, i)
 
 
-
+    # Save/Load network.
     def save(self, path='net/nn'):
         tf.train.Saver().save(self.sess, path)
 
@@ -83,61 +97,80 @@ class Network(object):
 
 class Data(object):
     def __init__(self):
-        pass
-
-    def loadData(self, path):
         self.input = []
         self.output = []
+        self.dataSize = 0
+
+    def loadData(self, path):
         dataSize = 0
         with open(path, 'r') as f:
-            while True:
+            while f.readline().rstrip() != '':
                 x = []
                 y = []
-                index = f.readline().rstrip()
-                if index == '':
-                    break
                 dataSize += 1
-                filters = []
+                features = []
+
+                # Get input data
                 for i in range(3):
+                    # Three feautre map.
                     dx=[]
                     for l in range(19):
                         line = f.readline().rstrip()
                         dx.append([int(data) for data in line.split()])
-                    filters.append(dx)
+                    # Append 19x19 input to feature map.
+                    features.append(dx)
                     f.readline()
+                # Reshape data([3x19x19]->[19x19x3])
                 for r in range(19):
                     row = []
                     for c in range(19):
-                        row.append([filters[0][r][c], filters[1][r][c], filters[2][r][c]])
+                        row.append([features[0][r][c], features[1][r][c], features[2][r][c]])
                     x.append(row)
 
+                # Get output data
                 for l in range(19):
                     line = f.readline().rstrip()
                     for data in line.split():
                         y.append(int(data))
                 f.readline()
+
+                # Append data.
                 self.input.append(x)
                 self.output.append(y)
+
+                # Print inf
                 if dataSize % 10000 == 0:
-                    print(dataSize)
+                    print('Reading the', dataSize, 'th data.', sep=' ', end='\r')
+                    break
 
         self.dataSize = dataSize
+        print('Finish reading', dataSize, 'data.', sep=' ')
 
+    # Get data by batch.
     def getBatch(self, index, batchSize):
         x = self.input[index : index + batchSize]
         y = self.output[index : index + batchSize]
         return x, y
 
 
+    # Clear all data.
+    def clearData(self):
+        self.input = []
+        self.output = []
+        self.dataSize = 0
+
+
 def main():
-    d = Data()
-    d.loadData('data1.txt')
+    testData = Data()
+    testData.loadData('testdata.txt')
+    data = Data()
+    data.loadData('data1.txt')
 
     nn = Network()
     nn.setUp()
     nn.initVar()
     for i in range(10):
-        nn.train(d)
+        nn.train(data, testData)
         nn.save()
 
 
