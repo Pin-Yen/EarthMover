8331a332630b7a4e374efb0d2c67004541463924
Todd <chihsuanwu@outlook.com>
2017-09-02 18:26:00 +0800

    update cnntrainer.py

--- neuralnetwork/go/cnntrainer.py
+++ neuralnetwork/go/cnntrainer.py
@@ -8,15 +8,15 @@ class Network(object):
         # Network's depth and width.
         self.depth = depth
         self.width = width
-        self.miniBatch = 0
+        self.global_batch = 0
 
     # Set up network structure.
-    def setUp(self):
+    def set_up(self):
         # Input: 19x19x3, output: 361.
         self.x = tf.placeholder(tf.float32, [None, 19, 19, 3])
         self.y = tf.placeholder(tf.float32, [None, 361])
 
-        # Each layers is composed of conv and bn layer
+        # Each layers excpet last layer is composed of conv and bn layer
         # First layer.
         with tf.name_scope('Layer1'):
             conv1 = tf.layers.conv2d(
@@ -25,70 +25,76 @@ class Network(object):
             bn1 = tf.layers.batch_normalization(conv1, name='bn1')
 
         # Hidden layer.
-        currentLayer = bn1
+        current_layer = bn1
         for i in range(2, self.depth + 1):
             with tf.name_scope('Layer' + str(i)):
-                conv = tf.layers.conv2d(currentLayer, self.width, 3,
-                    padding = 'same', activation = tf.nn.relu, name='conv' + str(i))
+                conv = tf.layers.conv2d(current_layer, self.width, 3,
+                                        padding='same', activation=tf.nn.relu,
+                                        name='conv' + str(i))
                 bn = tf.layers.batch_normalization(conv, name='bn' + str(i))
-            currentLayer = bn
+            current_layer = bn
 
-        # Output layer.
+        # Last layer.
         with tf.name_scope('Layer' + str(self.depth + 1)):
-            conv1x1 = tf.layers.conv2d(currentLayer, 1, 1,
-                padding = 'same', activation = tf.nn.relu, name='conv1x1')
-            output = tf.nn.softmax(tf.reshape(conv1x1, [-1, 361]))
+            conv1x1 = tf.layers.conv2d(current_layer, 1, 1,
+                padding='same', activation=tf.nn.relu, name='conv1x1')
+            logits = tf.reshape(conv1x1, [-1, 361])
+
+        # Softmax output.
+        with tf.name_scope('Output'):
+            self.output = tf.nn.softmax(logits, name='output')
 
         # Loss function.
         with tf.name_scope('Loss'):
-            self.loss = tf.losses.softmax_cross_entropy(
-                onehot_labels = self.y, logits = output)
-            tf.summary.scalar('loss', self.loss)
+            self.loss = tf.reduce_mean(
+                tf.nn.softmax_cross_entropy_with_logits(logits=logits,
+                                                        labels=self.y))
+        tf.summary.scalar('loss', self.loss)
 
         # Optimizer.
+        #self.opt = tf.train.RMSPropOptimizer(1e-3).minimize(self.loss)
         self.opt = tf.train.AdamOptimizer(1e-4).minimize(self.loss)
 
         # Accuracy.
         with tf.name_scope('Accuracy'):
-            correct_prediction = tf.equal(tf.argmax(output, 1),
-                                          tf.argmax(self.y, 1))
-            self.accuracy = tf.reduce_mean(
-                tf.cast(correct_prediction, tf.float32))
-            tf.summary.scalar('accuracy', self.accuracy)
+            correct = tf.equal(tf.argmax(logits, 1), tf.argmax(self.y, 1))
+            self.accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+        tf.summary.scalar('accuracy', self.accuracy)
 
         # Summary.
         self.merged = tf.summary.merge_all()
-        self.trainWriter = tf.summary.FileWriter("logs/train", self.sess.graph)
-        self.testWriter = tf.summary.FileWriter("logs/test", self.sess.graph)
-
+        self.train_writer = tf.summary.FileWriter(
+            "logs/train", self.sess.graph)
+        self.test_writer = tf.summary.FileWriter(
+            "logs/test", self.sess.graph)
 
     # Initialize variable.
-    def initVar(self):
+    def init_var(self):
         self.sess.run(tf.global_variables_initializer())
 
-
     # Train network.
-    def train(self, trainData, testData=None, batchSize=128):
-        for i in range(trainData.dataSize // batchSize):
-            self.miniBatch += 1
-            print('mini batch ' + str(self.miniBatch), end='\r')
+    def train(self, train_data, test_data, batch_size):
+        for i in range(train_data.data_size // batch_size):
+            print('\rbatch ' + str(self.global_batch), end='')
             # Get in/out data.
-            x, y = trainData.getBatch(i, batchSize)
+            x, y = train_data.get_data(i * batch_size, batch_size)
+            # Train network.
             self.sess.run([self.opt], {self.x: x, self.y: y})
 
             # Upgrade accuracy to summary.
-            if i % 10 == 0:
-                summ = self.sess.run(self.merged,
-                        {self.x: x, self.y: y})
+            if self.global_batch % 10 == 0:
+                # Accuracy of train data.
+                summ = self.sess.run(
+                    self.merged, {self.x: x, self.y: y})
+                self.train_writer.add_summary(summ, self.global_batch)
+                # Accuracy of test data.
+                x, y = test_data.get_random_data(batch_size)
+                testSumm = self.sess.run(
+                    self.merged, {self.x: x, self.y: y})
+                self.test_writer.add_summary(testSumm, self.global_batch)
 
-                self.trainWriter.add_summary(summ, self.miniBatch)
-                if testData != None:
-                    testX, testY = testData.getBatch(0, 256)
-                    testSumm = self.sess.run(self.merged,
-                        {self.x: testX, self.y: testY})
-
-                    self.testWriter.add_summary(testSumm, self.miniBatch)
-
+            # Increase global batch counter
+            self.global_batch += 1
 
     # Save/Load network.
     def save(self, path='net/nn'):
@@ -100,17 +106,17 @@ class Network(object):
 
 class Data(object):
     def __init__(self):
-        self.input = []
-        self.output = []
-        self.dataSize = 0
+        self.data = []
+        self.data_size = 0
 
-    def loadData(self, path):
-        dataSize = 0
+    def load_data(self, path, max_data=0):
+        print('Reading data from ' + path)
+        data_size = 0
         with open(path, 'r') as f:
             while f.readline().rstrip() != '':
                 x = []
                 y = []
-                dataSize += 1
+                data_size += 1
                 features = []
 
                 # Get input data
@@ -138,49 +144,76 @@ class Data(object):
                 f.readline()
 
                 # Append data.
-                self.input.append(x)
-                self.output.append(y)
+                self.data.append((x, y))
 
                 # Print inf
-                if dataSize % 10000 == 0:
-                    print('Reading the', dataSize, 'th data.', sep=' ', end='\r')
+                if data_size % 10000 == 0:
+                    print('Reading the ' + str(data_size) +
+                          ' th data.', end='\r')
 
-        self.dataSize = dataSize
-        print('Finish reading', dataSize, 'data.', sep=' ')
+                if max_data > 0:
+                    if data_size == max_data:
+                        break
+
+        self.data_size = data_size
+        print('Finish reading ' + str(data_size) + ' data.')
 
     # Get data by batch.
-    def getBatch(self, index, batchSize):
-        x = self.input[index : index + batchSize]
-        y = self.output[index : index + batchSize]
+    def get_data(self, index, batch_size):
+        x, y = zip(*(self.data[index : index + batch_size]))
         return x, y
 
+    # Get a batch of random data.
+    def get_random_data(self, batch_size):
+        data = random.sample(self.data, batch_size)
+        x, y = zip(*data)
+        return x, y
 
     # Clear all data.
-    def clearData(self):
-        self.input = []
-        self.output = []
-        self.dataSize = 0
+    def clear_data(self):
+        self.data = []
+        self.data_size = 0
 
-    def shuffleData(self):
-        data = list(zip(self.input, self.output))
-        random.shuffle(data)
-        self.input, self.output = zip(*data)
+    # Shuffle data.
+    def shuffle(self):
+        random.shuffle(self.data)
 
+
+# Train multiple data, 'train_data_path' should be a list of data path
+def train_multi_input(network, train_data_path, test_data_path, epoch):
+    epoch_counter = 0
+    train_data = Data()
+    test_data = Data()
+    test_data.load_data(test_data_path)
+    while epoch_counter < epoch:
+        for data_path in train_data_path:
+            train_data.clear_data()
+            train_data.load_data(data_path)
+            train_data.shuffle()
+            network.train(train_data, test_data, 128)
+            epoch_counter += 1
+            print('epoch ' + str(epoch_counter))
+            network.save('net/nn_epoch' + str(epoch_counter))
+            if epoch_counter == epoch:
+                return
+
+def train_single_input(network, train_data_path, test_data_path, epoch):
+    train_data = Data()
+    train_data.load_data(train_data_path)
+    test_data = Data()
+    test_data.load_data(test_data_path)
+    for i in range(epoch):
+        train_data.shuffle()
+        network.train(train_data, test_data, 128)
+        print('epoch ' + str(i + 1))
+        network.save('net/nn_epoch' + str(i + 1))
 
 def main():
-    testData = Data()
-    testData.loadData('data_2863_01.txt')
-    data = Data()
-    data.loadData('data_135724_025.txt')
+    network = Network()
+    network.set_up()
+    network.init_var()
 
-    nn = Network()
-    nn.setUp()
-    nn.initVar()
-    for i in range(10):
-        nn.train(data, testData)
-        nn.save()
-        data.shuffleData()
-
+    train_single_input(network, 'data_135724_025.txt', 'data_2863_01.txt', 20)
 
 
 if __name__ == '__main__':
